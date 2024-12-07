a7440aaa0926693ed4694ac0da4ab8d7c476e58f
Matthew Denton <mattdenton94@gmail.com>
2024-12-07 17:07:12 +0200

    New page created with form

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -6,4 +6,15 @@ class DogsController < ApplicationController
   def show
     @dog = Dog.find(params[:id])
   end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def create
+    @dog = Dog.new
+    @dog.save
+    redirect_to dogs_path(@dog)
+  end
+
 end
