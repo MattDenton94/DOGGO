10f3f418b9e397bc7c7e6e47ee5c7846b8883bcf
Matthew Denton <mattdenton94@gmail.com>
2024-12-11 18:49:51 +0200

    homepage copy done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -8,18 +8,21 @@ class DogsController < ApplicationController
   end
 
   def new
-    @dog = Dog.new
+    @dog = Dog.new(user_id: current_user.id)
   end
 
   def create
     @dog = Dog.new(dog_params)
-    @dog.save
-    redirect_to dogs_path(@dog)
+    if @dog.save
+      redirect_to dogs_path(@dog)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def dog_params
-    params.require(:dog).permit(:name, :age, :breed, :gender, :size, :temperament)
+    params.require(:dog).permit(:name, :age, :breed, :gender, :size, :temperament, :user_id)
   end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,11 @@
 class Dog < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :age, numericality: { only_integer: true }
+  validates :breed, presence: true
+  validates :gender, inclusion: { in: ["Male", "Female"] }
+  validates :size, inclusion: { in: ["Small", "Medium", "Large"] }
+  validates :temperament, presence: true
+  validates :user_id, presence: true
 end
