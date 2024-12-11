3f2c08ab1b9aa7c15d015f47e5713914ff339ebc
Matthew Denton <mattdenton94@gmail.com>
2024-12-11 09:01:35 +0200

    Edit the create action for dog

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -12,9 +12,17 @@ class DogsController < ApplicationController
   end
 
   def create
-    @dog = Dog.new()
-    @dog.save
-    redirect_to dogs_path(@dog)
+    @dog = current_user.dogs.new(dog_params)
+    if @dog.save
+      redirect_to @dog, notice: "Dog added successfully."
+    else
+      render :new
+    end
   end
 
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :age, :breed, :gender, :size, :temperament)
+  end
 end
