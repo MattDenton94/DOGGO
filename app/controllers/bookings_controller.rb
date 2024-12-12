46fbe2c4ba3b80c473566a1fa54e35257a5d5b10
Matthew Denton <mattdenton94@gmail.com>
2024-12-12 19:15:54 +0200

    edited navbar

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,22 @@
 class BookingsController < ApplicationController
   def index
-    @bookings = Booking.all
-  end
-
-  def show
-    @booking = Booking.find(params[:id])
+    @bookings = current_user.bookings
   end
 
   def new
+    @dog = Dog.find(params[:dog_id])
+    @booking = @dog.bookings.new
   end
 
   def create
+    @dog = Dog.find(params[:dog_id])
+    @booking = @dog.bookings.new(booking_params)
+    @booking.user = current_user
+    if @booking.save
+      redirect_to bookings_path, notice: "Booking created successfully."
+    else
+      render :new
+    end
   end
 
   def edit
@@ -20,5 +26,11 @@ class BookingsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
