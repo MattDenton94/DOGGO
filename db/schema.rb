3816f27e7734209c6b35bd777e4835e8d7ac0dc4
Matthew Denton <mattdenton94@gmail.com>
2024-12-14 17:12:56 +0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema[7.2].define(version: 2024_12_14_144221) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
